1df4c8c8968433e1537e3c8089a246532597ae59
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-11-13 10:12:46 +0900

    add forge client init

--- forge/src/main/java/net/pitan76/storageboxadapter/forge/StorageBoxAdapterForge.java
+++ forge/src/main/java/net/pitan76/storageboxadapter/forge/StorageBoxAdapterForge.java
@@ -2,6 +2,7 @@ package net.pitan76.storageboxadapter.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.pitan76.mcpitanlib.api.util.PlatformUtil;
 import net.pitan76.storageboxadapter.StorageBoxAdapter;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,5 +14,8 @@ public class StorageBoxAdapterForge {
 
         EventBuses.registerModEventBus(StorageBoxAdapter.MOD_ID, modEventBus);
         new StorageBoxAdapter();
+
+        if (PlatformUtil.isClient())
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(StorageBoxAdapterClientForge::clientInit);
     }
 }
